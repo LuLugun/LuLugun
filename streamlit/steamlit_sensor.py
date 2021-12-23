c3f79a3dcc1d9513d04879760b06df1ac4be36b6
A7215455林群峰 <53947178+LuLugun@users.noreply.github.com>
2021-12-23 22:01:25 +0800

    Create steamlit_sensor.py

---
+++ streamlit/steamlit_sensor.py
@@ -0,0 +1,58 @@
+import time
+import streamlit as st
+import datetime
+import numpy as np
+import pandas as pd
+from bokeh.plotting import figure, show
+
+sensor = pd.read_csv('sensor (1).csv')
+sensor=sensor.values
+sensor = pd.DataFrame(sensor)
+sensor.columns=['時間','溫度','濕度','水質(盆栽)','亮度','二氧化碳濃度','水溫(盆栽)','水溫(水池)']
+
+
+if st.checkbox('溫度'):
+    temperature = sensor['溫度']
+    temperature = pd.DataFrame(temperature)
+    temperature.set_index(pd.to_datetime(sensor["時間"],format="%Y/%m/%d %H:%M"),inplace=True)
+    line_chart = st.line_chart(temperature)    #折線圖
+        
+
+
+if st.checkbox('濕度'):
+    temperature = sensor['濕度']
+    temperature = pd.DataFrame(temperature)
+    temperature.set_index(pd.to_datetime(sensor["時間"],format="%Y/%m/%d %H:%M"),inplace=True)
+    line_chart = st.line_chart(temperature)    #折線圖
+
+if st.checkbox('水質(盆栽)'):
+    temperature = sensor['水質(盆栽)']
+    temperature = pd.DataFrame(temperature)
+    temperature.set_index(pd.to_datetime(sensor["時間"],format="%Y/%m/%d %H:%M"),inplace=True)
+    line_chart = st.line_chart(temperature)    #折線圖
+
+if st.checkbox('亮度'):
+    temperature = sensor['亮度']
+    temperature = pd.DataFrame(temperature)
+    temperature.set_index(pd.to_datetime(sensor["時間"],format="%Y/%m/%d %H:%M"),inplace=True)
+    line_chart = st.line_chart(temperature)    #折線圖
+
+if st.checkbox('二氧化碳濃度'):
+    temperature = sensor['二氧化碳濃度']
+    temperature = pd.DataFrame(temperature)
+    temperature.set_index(pd.to_datetime(sensor["時間"],format="%Y/%m/%d %H:%M"),inplace=True)
+    line_chart = st.line_chart(temperature)    #折線圖
+
+if st.checkbox('水溫(盆栽)'):
+    temperature = sensor['水溫(盆栽)']
+    temperature = pd.DataFrame(temperature)
+    temperature.set_index(pd.to_datetime(sensor["時間"],format="%Y/%m/%d %H:%M"),inplace=True)
+    line_chart = st.line_chart(temperature)    #折線圖
+
+if st.checkbox('水溫(水池)'):
+    temperature = sensor['水溫(水池)']
+    temperature = pd.DataFrame(temperature)
+    temperature.set_index(pd.to_datetime(sensor["時間"],format="%Y/%m/%d %H:%M"),inplace=True)
+    line_chart = st.line_chart(temperature)    #折線圖
+
+
