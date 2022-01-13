ac64b3563f5f95df6f5ccf948ee7403942aea452
LuLugun <steven.lin.7x7@gmail.com>
2022-01-13 08:38:59 +0800

    更改寫入規則

--- VDlive/VDLive.py
+++ VDlive/VDLive.py
@@ -7,12 +7,27 @@ import untangle, csv
 import os
 local_path = os.path.dirname(os.path.abspath(__file__))
 
+linkid_list = ['3000900115387U', '3000900116276U', '3000900116579U', '3000900117023U', '3000900117598U', '3000900117886U']
+
+def xml_to_csv_dict(vdid,linkid,laneid,lanetype,speed,occupancy,vehicletype,volume,speed2,status,datacollecttime):
+    data = {"vdid": str(vdid).replace(' ','').strip(),
+                        "linkid": str(linkid).replace(' ','').strip(),
+                        "laneid": str(laneid).replace(' ','').strip(),
+                        "lanetype": str(lanetype).replace(' ','').strip(),
+                        "speed": str(speed).replace(' ','').strip(),
+                        "occupancy": str(occupancy).replace(' ','').strip(),
+                        "vehicletype": str(vehicletype).replace(' ','').strip(),
+                        "volume": str(volume).replace(' ','').strip(),
+                        "speed2": str(speed2).replace(' ','').strip(),
+                        "status": str(status).replace(' ','').strip(),
+                        "datacollecttime": str(datacollecttime).replace(' ','').strip().replace('T',' ').replace('+08:00','')}
+    return data
+print('自動抓取xml轉csv啟動')
 while True:
     try:
         content = urllib.request.urlopen('https://thbapp.thb.gov.tw/opendata/vd/one/VDLiveList.xml')
         soup = BeautifulSoup(content,"html.parser")
         str_all = soup.prettify()
-        time.sleep(10)
         path = 'VDLiveList.xml'
         f = open(path, 'w')
         f.write(str_all)
@@ -33,6 +48,7 @@ while True:
                 datacollecttime = ''
             try:
                 linkid = html.linkflows.linkflow.linkid.cdata
+                linkid = str(linkid).replace(' ','').strip()
                 try:
                     laneid = html.linkflows.linkflow.lanes.lane.laneid.cdata  # 有多個laneid
                     lanetype = html.linkflows.linkflow.lanes.lane.lanetype.cdata
@@ -45,17 +61,8 @@ while True:
                             speed2 = html.linkflows.linkflow.lanes.lane.vehicles.vehicle.speed.cdata
                         except AttributeError as e:
                             speed2 = ""
-                        xml_to_csv.append({"vdid": str(vdid).replace(' ','').strip(),
-                                            "linkid": str(linkid).replace(' ','').strip(),
-                                            "laneid": str(laneid).replace(' ','').strip(),
-                                            "lanetype": str(lanetype).replace(' ','').strip(),
-                                            "speed": str(speed).replace(' ','').strip(),
-                                            "occupancy": str(occupancy).replace(' ','').strip(),
-                                            "vehicletype": str(vehicletype).replace(' ','').strip(),
-                                            "volume": str(volume).replace(' ','').strip(),
-                                            "speed2": str(speed2).replace(' ','').strip(),
-                                            "status": str(status).replace(' ','').strip(),
-                                            "datacollecttime": str(datacollecttime).replace(' ','').strip().replace('T',' ').replace('+08:00','')})
+                        if linkid in linkid_list:
+                            xml_to_csv.append(xml_to_csv_dict(vdid,linkid,laneid,lanetype,speed,occupancy,vehicletype,volume,speed2,status,datacollecttime))
                     except:
                         for html_vehicle in html.linkflows.linkflow.lanes.lane.vehicles.vehicle:
                             vehicletype = html_vehicle.vehicletype.cdata
@@ -64,17 +71,8 @@ while True:
                                 speed2 = html_vehicle.speed.cdata
                             except AttributeError as e:
                                 speed2 = ""
-                            xml_to_csv.append({"vdid": str(vdid).replace(' ','').strip(),
-                                            "linkid": str(linkid).replace(' ','').strip(),
-                                            "laneid": str(laneid).replace(' ','').strip(),
-                                            "lanetype": str(lanetype).replace(' ','').strip(),
-                                            "speed": str(speed).replace(' ','').strip(),
-                                            "occupancy": str(occupancy).replace(' ','').strip(),
-                                            "vehicletype": str(vehicletype).replace(' ','').strip(),
-                                            "volume": str(volume).replace(' ','').strip(),
-                                            "speed2": str(speed2).replace(' ','').strip(),
-                                            "status": str(status).replace(' ','').strip(),
-                                            "datacollecttime": str(datacollecttime).replace(' ','').strip().replace('T',' ').replace('+08:00','')})
+                            if linkid in linkid_list:
+                                xml_to_csv.append(xml_to_csv_dict(vdid,linkid,laneid,lanetype,speed,occupancy,vehicletype,volume,speed2,status,datacollecttime))
 
                 except:
                     for html_laneid in html.linkflows.linkflow.lanes.lane:
@@ -89,17 +87,8 @@ while True:
                                 speed2 = html_laneid.vehicles.vehicle.speed.cdata
                             except AttributeError as e:
                                 speed2 = ""
-                            xml_to_csv.append({"vdid": str(vdid).replace(' ','').strip(),
-                                            "linkid": str(linkid).replace(' ','').strip(),
-                                            "laneid": str(laneid).replace(' ','').strip(),
-                                            "lanetype": str(lanetype).replace(' ','').strip(),
-                                            "speed": str(speed).replace(' ','').strip(),
-                                            "occupancy": str(occupancy).replace(' ','').strip(),
-                                            "vehicletype": str(vehicletype).replace(' ','').strip(),
-                                            "volume": str(volume).replace(' ','').strip(),
-                                            "speed2": str(speed2).replace(' ','').strip(),
-                                            "status": str(status).replace(' ','').strip(),
-                                            "datacollecttime": str(datacollecttime).replace(' ','').strip().replace('T',' ').replace('+08:00','')})
+                            if linkid in linkid_list:
+                                xml_to_csv.append(xml_to_csv_dict(vdid,linkid,laneid,lanetype,speed,occupancy,vehicletype,volume,speed2,status,datacollecttime))
                         except:
                             for html_vehicle in html_laneid.vehicles.vehicle:
                                 vehicletype = html_vehicle.vehicletype.cdata
@@ -108,21 +97,13 @@ while True:
                                     speed2 = html_vehicle.speed.cdata
                                 except AttributeError as e:
                                     speed2 = ""
-                                xml_to_csv.append({"vdid": str(vdid).replace(' ','').strip(),
-                                            "linkid": str(linkid).replace(' ','').strip(),
-                                            "laneid": str(laneid).replace(' ','').strip(),
-                                            "lanetype": str(lanetype).replace(' ','').strip(),
-                                            "speed": str(speed).replace(' ','').strip(),
-                                            "occupancy": str(occupancy).replace(' ','').strip(),
-                                            "vehicletype": str(vehicletype).replace(' ','').strip(),
-                                            "volume": str(volume).replace(' ','').strip(),
-                                            "speed2": str(speed2).replace(' ','').strip(),
-                                            "status": str(status).replace(' ','').strip(),
-                                            "datacollecttime": str(datacollecttime).replace(' ','').strip().replace('T',' ').replace('+08:00','')})
+                                if linkid in linkid_list:
+                                    xml_to_csv.append(xml_to_csv_dict(vdid,linkid,laneid,lanetype,speed,occupancy,vehicletype,volume,speed2,status,datacollecttime))
                         
             except:
                 for html_linkid in html.linkflows.linkflow:
                     linkid = html_linkid.linkid.cdata
+                    linkid = str(linkid).replace(' ','').strip()
                     try:
                         laneid = html_linkid.lanes.lane.laneid.cdata
                         lanetype = html_linkid.lanes.lane.lanetype.cdata
@@ -135,17 +116,8 @@ while True:
                                 speed2 = html_linkid.lanes.lane.vehicles.vehicle.speed.cdata
                             except AttributeError as e:
                                 speed2 = ""
-                            xml_to_csv.append({"vdid": str(vdid).replace(' ','').strip(),
-                                            "linkid": str(linkid).replace(' ','').strip(),
-                                            "laneid": str(laneid).replace(' ','').strip(),
-                                            "lanetype": str(lanetype).replace(' ','').strip(),
-                                            "speed": str(speed).replace(' ','').strip(),
-                                            "occupancy": str(occupancy).replace(' ','').strip(),
-                                            "vehicletype": str(vehicletype).replace(' ','').strip(),
-                                            "volume": str(volume).replace(' ','').strip(),
-                                            "speed2": str(speed2).replace(' ','').strip(),
-                                            "status": str(status).replace(' ','').strip(),
-                                            "datacollecttime": str(datacollecttime).replace(' ','').strip().replace('T',' ').replace('+08:00','')})
+                            if linkid in linkid_list:
+                                xml_to_csv.append(xml_to_csv_dict(vdid,linkid,laneid,lanetype,speed,occupancy,vehicletype,volume,speed2,status,datacollecttime))
                         except:
                             for html_vehicle in html_linkid.lanes.lane.vehicles.vehicle:
                                 vehicletype = html_vehicle.vehicletype.cdata
@@ -154,17 +126,8 @@ while True:
                                     speed2 = html_vehicle.speed.cdata
                                 except AttributeError as e:
                                     speed2 = ""
-                                xml_to_csv.append({"vdid": str(vdid).replace(' ','').strip(),
-                                            "linkid": str(linkid).replace(' ','').strip(),
-                                            "laneid": str(laneid).replace(' ','').strip(),
-                                            "lanetype": str(lanetype).replace(' ','').strip(),
-                                            "speed": str(speed).replace(' ','').strip(),
-                                            "occupancy": str(occupancy).replace(' ','').strip(),
-                                            "vehicletype": str(vehicletype).replace(' ','').strip(),
-                                            "volume": str(volume).replace(' ','').strip(),
-                                            "speed2": str(speed2).replace(' ','').strip(),
-                                            "status": str(status).replace(' ','').strip(),
-                                            "datacollecttime": str(datacollecttime).replace(' ','').strip().replace('T',' ').replace('+08:00','')})
+                                if linkid in linkid_list:
+                                    xml_to_csv.append(xml_to_csv_dict(vdid,linkid,laneid,lanetype,speed,occupancy,vehicletype,volume,speed2,status,datacollecttime))
                     except:
                         for html_laneid in html_linkid.lanes.lane:
                             laneid = html_laneid.laneid.cdata
@@ -178,17 +141,8 @@ while True:
                                     speed2 = html_laneid.vehicles.vehicle.speed.cdata
                                 except AttributeError as e:
                                     speed2 = ""
-                                xml_to_csv.append({"vdid": str(vdid).replace(' ','').strip(),
-                                            "linkid": str(linkid).replace(' ','').strip(),
-                                            "laneid": str(laneid).replace(' ','').strip(),
-                                            "lanetype": str(lanetype).replace(' ','').strip(),
-                                            "speed": str(speed).replace(' ','').strip(),
-                                            "occupancy": str(occupancy).replace(' ','').strip(),
-                                            "vehicletype": str(vehicletype).replace(' ','').strip(),
-                                            "volume": str(volume).replace(' ','').strip(),
-                                            "speed2": str(speed2).replace(' ','').strip(),
-                                            "status": str(status).replace(' ','').strip(),
-                                            "datacollecttime": str(datacollecttime).replace(' ','').strip().replace('T',' ').replace('+08:00','')})
+                                if linkid in linkid_list:
+                                    xml_to_csv.append(xml_to_csv_dict(vdid,linkid,laneid,lanetype,speed,occupancy,vehicletype,volume,speed2,status,datacollecttime))
                             except:
                                 for html_vehicle in html_laneid.vehicles.vehicle:
                                     vehicletype = html_vehicle.vehicletype.cdata
@@ -197,17 +151,8 @@ while True:
                                         speed2 = html_vehicle.speed.cdata
                                     except AttributeError as e:
                                         speed2 = ""
-                                xml_to_csv.append({"vdid": str(vdid).replace(' ','').strip(),
-                                            "linkid": str(linkid).replace(' ','').strip(),
-                                            "laneid": str(laneid).replace(' ','').strip(),
-                                            "lanetype": str(lanetype).replace(' ','').strip(),
-                                            "speed": str(speed).replace(' ','').strip(),
-                                            "occupancy": str(occupancy).replace(' ','').strip(),
-                                            "vehicletype": str(vehicletype).replace(' ','').strip(),
-                                            "volume": str(volume).replace(' ','').strip(),
-                                            "speed2": str(speed2).replace(' ','').strip(),
-                                            "status": str(status).replace(' ','').strip(),
-                                            "datacollecttime": str(datacollecttime).replace(' ','').strip().replace('T',' ').replace('+08:00','')})
+                                    if linkid in linkid_list:
+                                        xml_to_csv.append(xml_to_csv_dict(vdid,linkid,laneid,lanetype,speed,occupancy,vehicletype,volume,speed2,status,datacollecttime))
 
             
         df = pd.DataFrame(xml_to_csv, columns=title)   
