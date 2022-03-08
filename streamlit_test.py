ab33bf2f3df49b3a4a1ca6059402ab121a8e3c4a
A7215455林群峰 <53947178+LuLugun@users.noreply.github.com>
2022-03-08 11:40:26 +0800

    Update streamlit_test.py

--- streamlit_test.py
+++ streamlit_test.py
@@ -1,14 +1,39 @@
+import time
 import streamlit as st
-from bokeh.plotting import figure
+import datetime
+import numpy as np
+import pandas as pd
+import pymysql
+import os
 
-x = [1, 2, 3, 4, 5]
-y = [6, 7, 2, 4, 5]
+def sql_connect():
+    global db,cursor,host
+    host='8.tcp.ngrok.io'
+    port = 14092
+    user='a11805'
+    passwd='pccua11805'
+    database='aiot'
+    print('Connecting....')
+    print('host:%s\nuser:%s\npassword:********\ndatabase:%s\n'%(host,user,database))
+    db=pymysql.connect(host=host,user=user,passwd=passwd,database=database,port=port)
+    os.system('clear')
+    cursor=db.cursor()
+    print('Connection succeed')
 
-p = figure(
-     title='simple line example',
-     x_axis_label='x',
-     y_axis_label='y')
+def select_data(title_name,table_name,number):
+    sql = '''SELECT '''+title_name+''' FROM '''+table_name+''' ORDER BY `time` DESC LIMIT '''+number+''';'''
+    print(sql)
+    cursor.execute(sql)
+    result=cursor.fetchall()
+    result = pd.DataFrame(result)
+    return result
 
-p.line(x, y, legend_label='Trend', line_width=2)
+sql_connect()
+sensor = select_data('''`time`, `temperature`, `humidity`, `quality_Potted`, `quality_Reservoir`, `luminance`, `CO2`, `Potted`, `Reservoir`, `smoke`''','sensor_all','10')
+sensor.columns=['時間','溫度','濕度','水質(盆栽)','水質(水池)','亮度','CO2','水溫','水溫(水池)','煙霧']
+print(sensor)
 
-st.bokeh_chart(p, use_container_width=True)
+temperature = sensor
+temperature = pd.DataFrame(temperature)
+temperature.set_index(pd.to_datetime(sensor["時間"],format="%Y-%m-%d %H:%M:%S"),inplace=True)
+line_chart = st.line_chart(temperature,use_container_width = False)
