733acf4cee74f0013a4b2badfa5aaa463c30e8df
A7215455林群峰 <53947178+LuLugun@users.noreply.github.com>
2022-03-08 12:35:16 +0800

    Update streamlit_test.py

--- streamlit_test.py
+++ streamlit_test.py
@@ -31,13 +31,26 @@ def select_data(title_name,table_name,number):
 sql_connect()
 sensor = select_data('''`time`, `temperature`, `humidity`, `quality_Potted`, `quality_Reservoir`, `luminance`, `CO2`, `Potted`, `Reservoir`, `smoke`''','sensor_all','2016')
 sensor.columns=['時間','溫度','濕度','水質(盆栽)','水質(水池)','亮度','CO2','水溫','水溫(水池)','煙霧']
-print(sensor)
+
+
 line_time = sensor["時間"]
-temperature = sensor
 
-temperature.drop(columns = '時間',inplace=True)
+sensor_all = sensor
+sensor_all.drop(columns = '時間',inplace=True)
+sensor_all = pd.DataFrame(sensor_all)
+sensor_all.set_index(pd.to_datetime(line_time,format="%Y-%m-%d %H:%M:%S"),inplace=True)
+line_chart = st.line_chart(sensor_all)
 
-temperature = pd.DataFrame(temperature)
-temperature.set_index(pd.to_datetime(line_time,format="%Y-%m-%d %H:%M:%S"),inplace=True)
-
-line_chart = st.line_chart(temperature)
+col1, col2  = st.columns(2)
+col3, col4  = st.columns(2)
+col5, col6  = st.columns(2)
+if col1.checkbox('溫度'):
+    temperature = sensor['溫度']
+    temperature = pd.DataFrame(temperature)
+    temperature.set_index(pd.to_datetime(line_time,format="%Y-%m-%d %H:%M:%S"),inplace=True)
+    line_chart = col1.line_chart(temperature)
+if col2.checkbox('濕度'):
+    humidity = sensor['濕度']
+    humidity = pd.DataFrame(humidity)
+    humidity.set_index(pd.to_datetime(line_time,format="%Y-%m-%d %H:%M:%S"),inplace=True)
+    line_chart = col2.line_chart(humidity)    #折線圖
