760d81dfb592602d47cd86967a1b9a5bf0e6ad82
A7215455林群峰 <53947178+LuLugun@users.noreply.github.com>
2022-03-08 12:39:49 +0800

    Update streamlit_test.py

--- streamlit_test.py
+++ streamlit_test.py
@@ -54,3 +54,26 @@ if col2.checkbox('濕度'):
     humidity = pd.DataFrame(humidity)
     humidity.set_index(pd.to_datetime(line_time,format="%Y-%m-%d %H:%M:%S"),inplace=True)
     line_chart = col2.line_chart(humidity,height = 200,use_container_width = False)    #折線圖
+if col3.checkbox('水質(盆栽)'):
+    quality_Potted = sensor['水質(盆栽)']
+    quality_Potted = pd.DataFrame(quality_Potted)
+    quality_Potted.set_index(pd.to_datetime(line_time,format="%Y-%m-%d %H:%M:%S"),inplace=True)
+    line_chart = col3.line_chart(quality_Potted,height = 200,use_container_width = False)    #折線圖
+
+if col4.checkbox('亮度'):
+    quality_Reservoir = sensor['亮度']
+    quality_Reservoir = pd.DataFrame(quality_Reservoir)
+    quality_Reservoir.set_index(pd.to_datetime(line_time,format="%Y-%m-%d %H:%M:%S"),inplace=True)
+    line_chart = col4.line_chart(quality_Reservoir,height = 200,use_container_width = False)    #折線圖
+
+if col5.checkbox('CO2'):
+    CO2 = sensor['CO2']
+    CO2 = pd.DataFrame(CO2)
+    CO2.set_index(pd.to_datetime(line_time,format="%Y-%m-%d %H:%M:%S"),inplace=True)
+    line_chart = col5.line_chart(CO2,height = 200,use_container_width = False)    #折線圖
+
+if col6.checkbox('水溫'):
+    Potted = sensor['水溫']
+    Potted = pd.DataFrame(Potted)
+    Potted.set_index(pd.to_datetime(line_time,format="%Y-%m-%d %H:%M:%S"),inplace=True)
+    line_chart = col6.line_chart(Potted,height = 200,use_container_width = False)    #折線圖
