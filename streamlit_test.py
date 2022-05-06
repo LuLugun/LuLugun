41b284ff98b84cd00bba90c56fd1040d772accc7
A7215455林群峰 <53947178+LuLugun@users.noreply.github.com>
2022-05-06 21:05:33 +0800

    Update streamlit_test.py

--- streamlit_test.py
+++ streamlit_test.py
@@ -38,9 +38,10 @@ result=cursor.fetchone()
 
 sensor = select_data('''`time`, `temperature`, `humidity`, `quality_Potted`, `quality_Reservoir`, `luminance`, `CO2`, `Potted`, `Reservoir`, `smoke`''','sensor_all','2016')
 sensor.columns=['時間','溫度','濕度','水質(盆栽)','水質(水池)','亮度','CO2','水溫','水溫(水池)','煙霧']
-
-st.subheader('智慧溫室中控台')
-
+col1, col2 ,col3  = st.columns(3)
+col1.subheader(' ')
+col2.subheader('智慧溫室中控台')
+col3.subheader(' ')
 col1, col2 ,col3  = st.columns(3)
 col1.metric(label="溫度", value=str(result[1])+" °c",)
 col2.metric(label="濕度", value=str(result[2])+" %",)
